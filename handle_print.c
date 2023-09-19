942734fc363ee4ec842d35f320e1f411081a004c
suadcode <suadsalam7@gmail.com>
2023-09-19 11:47:02 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -14,7 +14,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i, unknow_len = 0, printed_chars = -1;
+int i, unknown_length = 0, printed_chars = -1;
 fmt_t fmt_types[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
 {'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -29,9 +29,9 @@ if (fmt_types[i].fmt == '\0')
 {
 if (fmt[*ind] == '\0')
 return (-1);
-unknow_len += write(1, "%%", 1);
+unknown_length += write(1, "%%", 1);
 if (fmt[*ind - 1] == ' ')
-unknow_len += write(1, " ", 1);
+unknown_length += write(1, " ", 1);
 else if (width)
 {
 --(*ind);
@@ -41,8 +41,9 @@ if (fmt[*ind] == ' ')
 --(*ind);
 return (1);
 }
-unknow_len += write(1, &fmt[*ind], 1);
-return (unknow_len);
+unknown_length += write(1, &fmt[*ind], 1);
+return (unknown_length);
 }
 return (printed_chars);
 }
+
