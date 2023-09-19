5417c5a946ee3ce9ed55adc2e6e6ebf8a9882e70
suadcode <suadsalam7@gmail.com>
2023-09-19 11:27:45 -0700

    update

--- get_flags.c
+++ get_flags.c
@@ -10,7 +10,7 @@ int get_flags(const char *format, int *i)
 /* - + 0 # ' ' */
 /* 1 2 4 8  16 */
 int j, curr_i;
-int flags = 0;
+int flags_spec = 0;
 const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
@@ -18,12 +18,13 @@ for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 for (j = 0; FLAGS_CH[j] != '\0'; j++)
 if (format[curr_i] == FLAGS_CH[j])
 {
-flags |= FLAGS_ARR[j];
+flags_spec |= FLAGS_ARR[j];
 break;
 }
 if (FLAGS_CH[j] == 0)
 break;
 }
 *i = curr_i - 1;
-return (flags);
+return (flags_spec);
 }
+
